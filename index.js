f15b872c9fe22f2375102326d903530846b346e5
yaitaimo <yaitaimo@gmail.com>
2021-07-05 10:51:55 +0900

    ログイン経路を企業から個人に変更

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ const argv = yargs
   .help()
   .argv
 
-if(!process.env.OFFICE_ACCOUNT_NAME) {
-  throw new Error('OFFICE_ACCOUNT_NAME is not defined. Please check env file.')
-}
-
 if(!process.env.ACCOUNT_NAME_OR_EMAIL) {
   throw new Error('ACCOUNT_NAME_OR_EMAIL is not defined. Please check env file.')
 }
@@ -36,15 +32,36 @@ const getRandomInt = (min, max) => {
 }
 
 const getInTime = () => {
-  const minute = getRandomInt(0, 59).toString().padStart(2, '0');
+  const minute = getRandomInt(30, 59).toString().padStart(2, '0');
   const hour = getRandomInt(9, 10).toString().padStart(2, '0');
 	return hour + ':' + minute;
 }
 
 const getOutTime = () => {
   const minute = getRandomInt(0, 59).toString().padStart(2, '0');
-  const hour = getRandomInt(19, 21).toString();
+  const hour = getRandomInt(19, 20).toString();
 	return hour + ':' + minute;
+}
+
+const editAttendanceForm = async (page) => {
+    const date = await page.$$eval('.attendance-table-header-title', list => {
+      return list[1].textContent;
+    })
+    const addButton = await page.$('.attendance-with-plus-icon-normally');
+    await addButton.click();
+    await page.waitFor(200);
+    await addButton.click();
+    await page.waitFor(200);
+
+    const inTime = getInTime();
+    const outTime = getOutTime();
+    await page.select('select[name="attendance_form[attendance_record_forms_attributes][1][event]"', 'clock_out');
+    await page.type('input[name="attendance_form[attendance_record_forms_attributes][0][time]"]', inTime);
+    await page.waitFor(200);
+    await page.type('input[name="attendance_form[attendance_record_forms_attributes][1][time]"]', outTime);
+
+    console.log(`${date}: ${inTime} - ${outTime}`);
+    await page.click('input[name="commit"]');
 }
 
 (async () => {
@@ -53,16 +70,23 @@ const getOutTime = () => {
   await page.setViewport({width: 1600, height: 1200});
   await page.goto(targetUrl, {waitUntil: "domcontentloaded"});
 
-  await page.click('.attendance-button-email.attendance-button-link.attendance-button-size-wide');
+  await page.click('.attendance-button-mfid.attendance-button-link.attendance-button-size-wide')
   await page.waitFor(1000);
 
-  const submitButtonSelector = 'input[type="submit"]';
-  await page.waitForSelector(submitButtonSelector);
-  await page.type('#employee_session_form_office_account_name', process.env.OFFICE_ACCOUNT_NAME);
-  await page.type('#employee_session_form_account_name_or_email', process.env.ACCOUNT_NAME_OR_EMAIL);
-  await page.type('#employee_session_form_password', process.env.ACCOUNT_PASSWORD);
-  await page.click(submitButtonSelector);
+  await page.type('input[name="mfid_user[email]"', process.env.ACCOUNT_NAME_OR_EMAIL)
+  await page.click('input.submitBtn')
+
   await page.waitFor(1000);
+
+  await page.type('input[name="mfid_user[password]"]', process.env.ACCOUNT_PASSWORD)
+  await page.click('input.submitBtn')
+
+  await page.waitFor(5000);
+
+  await page.goto(targetUrl, {waitUntil: "domcontentloaded"});
+  await page.waitFor(90000);
+  const closeButton = await page.$('.karte-close');
+  await closeButton.click();
 
   while(true) {
     await page.waitFor(5000);
@@ -72,27 +96,17 @@ const getOutTime = () => {
     const row = errorRows[0]
     const editElement = await row.$('.column-edit a');
     await editElement.click();
+
     await page.waitFor(2000);
 
-    const addButton = await page.$('.attendance-with-plus-icon-normally');
-    await addButton.click();
-    await page.waitFor(200);
-    await addButton.click();
-    await page.waitFor(200);
-
-    await page.select('select[name="attendance_form[attendance_record_forms_attributes][1][event]"', 'clock_out');
-    await page.type('input[name="attendance_form[attendance_record_forms_attributes][0][time]"]', getInTime());
-    await page.waitFor(200);
-    await page.type('input[name="attendance_form[attendance_record_forms_attributes][1][time]"]', getOutTime());
-
-    await page.click(submitButtonSelector);
+    await editAttendanceForm(page);
   }
 
   const shinseiButton = await page.$('.attendance-table-header-action a.attendance-button-primary#kt-attendance-approval-request-button');
   if (shinseiButton !== null) {
     await shinseiButton.click();
     await page.waitFor(2000);
-    await page.click(submitButtonSelector);
+    await page.click('input[name="commit"]');
     await page.waitFor(2000);
   } else {
     console.log(`勤怠承認申請ボタンがありません(${year}年${month}月分は申請済みではありませんか？)`)
